b63ff0cc66026e63d5b5a185e5896f8d30f10751
Younes <yrichi59@gmail.com>
2022-11-14 00:32:52 +0100

    ajout du scoring

--- human vs zombies/src/codingame.ts
+++ human vs zombies/src/codingame.ts
@@ -1,8 +1,48 @@
 
 export const isModeDev = true;
 
-export function readline(): string {
-    return "";
+export function readlineCustom(): string {
+    return isModeDev ? "": readline();
+}
+
+
+export interface StrategieHero {
+    apply(hero: EtatElementSimulation, humans: EtatElementSimulation[], zombies: EtatElementSimulation[]): void;
+
+}
+
+/**
+ * ALGO BETE ET MECHANT , le hero va juste se diriger vers l'humain le plus proche pour le proteger 
+ */
+export class StrategieHeroDefault implements StrategieHero {
+    apply(hero: EtatElementSimulation, humans: EtatElementSimulation[], zombies: EtatElementSimulation[]): void {
+
+
+        const humanAbleToSave: EtatElementSimulation[] = [];
+        humans.forEach(h => {
+            const nearestZombie = h.element.getElementLePlusProche(zombies.map(zombie => zombie.element));
+            if (!!nearestZombie && h.element.getNbTourByOtherElement(hero.element) <= nearestZombie.getNbTourByOtherElement(h.element)) {
+                humanAbleToSave.push(h);
+            }
+        })
+
+        if (!!humanAbleToSave?.length) {
+            const humanToSave = humanAbleToSave
+                .reduce((elementActual, elmentNext) =>
+                    elementActual.element.getDistanceManathanByElement(hero.element) <= elmentNext.element.getDistanceManathanByElement(hero.element)
+                        ? elementActual : elmentNext);
+
+            if (hero.element.getDistanceManathanByElement(humanToSave.element) <= hero.element.getDistanceHopMax()) {
+                hero.element.position.positionX = humanToSave.element.position.positionX;
+                hero.element.position.positionY = humanToSave.element.position.positionY;
+            } else {
+                const nextMove = hero.element.getNextHop(humanToSave.element);
+                hero.element.position.positionX = nextMove.position.positionX;
+                hero.element.position.positionY = nextMove.position.positionY;
+            }
+        }
+    }
+
 }
 
 /**
@@ -28,58 +68,59 @@ export enum Direction {
     BAS_DIAGONAL_DROITE,
 }
 
+const UNREACHABLE_VALUE = 10000000000;
 export class Element {
     type: TypeElement;
-    positionX: number;
-    positionY: number;
+    position: Position;
 
     constructor(x: number, y: number, typeElement?: TypeElement) {
-        this.positionX = x;
-        this.positionY = y;
+        this.position = new Position(x, y);
         this.type = typeElement != null ? typeElement : TypeElement.POSITION;
     }
 
     getCoordonneeString(): string {
-        return '' + this.positionX + ' ' + this.positionY;
+        return '' + this.position.positionX + ' ' + this.position.positionY;
     }
 
     getNbTourByOtherElement(element: Element) {
-        const distance = this.getDistanceManathan(element.positionX, element.positionY);
+        const distance = this.getDistanceManathan(element.position.positionX, element.position.positionY);
         const nbTour = element.type === TypeElement.HERO ? (distance - 2000) / 1000 : distance / 400;
-        return nbTour % 1 != 0 ? nbTour + 1 : nbTour;
+        return Math.ceil(nbTour % 1 != 0 ? nbTour + 1 : nbTour);
     }
 
     getDistanceManathanByElement(element: Element) {
-        return this.getDistanceManathan(element.positionX, element.positionY);
+        return this.getDistanceManathan(element.position.positionX, element.position.positionY);
     }
 
     getDistanceManathan(positionX, positionY) {
-        return Math.sqrt(Math.abs(Math.pow(positionX - this.positionX, 2) + Math.pow(positionY - this.positionY, 2)));
+        return Math.sqrt(Math.abs(Math.pow(positionX - this.position.positionX, 2) + Math.pow(positionY - this.position.positionY, 2)));
     }
 
     getDistanceManathanByDirection(element: Element, direction: Direction): number {
-        return this.getDistanceManathan(this.getNeigbourElementByDirection(element, direction).positionX, this.getNeigbourElementByDirection(element, direction).positionY)
+        return this.getDistanceManathan(
+            this.getNeigbourElementByDirection(element, direction).position.positionX,
+            this.getNeigbourElementByDirection(element, direction).position.positionY)
     }
 
     getNeigbourElementByDirection(element: Element, direction: Direction): Element {
         switch (Number(direction)) {
             case (Direction.HAUT_DIAGONAL_GAUCHE):
-                return new Element(element.positionX - 1, element.positionY + 1);
+                return new Element(element.position.positionX - 1, element.position.positionY + 1);
             case (Direction.GAUCHE):
-                return new Element(element.positionX - 1, element.positionY);
+                return new Element(element.position.positionX - 1, element.position.positionY);
             case (Direction.BAS_DIAGONAL_GAUCHE):
-                return new Element(element.positionX - 1, element.positionY - 1);
+                return new Element(element.position.positionX - 1, element.position.positionY - 1);
             case (Direction.HAUT):
-                return new Element(element.positionX, element.positionY + 1);
+                return new Element(element.position.positionX, element.position.positionY + 1);
             case (Direction.BAS):
-                return new Element(element.positionX, element.positionY - 1);
+                return new Element(element.position.positionX, element.position.positionY - 1);
             case (Direction.HAUT_DIAGONAL_DROITE):
-                return new Element(element.positionX + 1, element.positionY + 1);
+                return new Element(element.position.positionX + 1, element.position.positionY + 1);
             case (Direction.DROITE):
-                return new Element(element.positionX + 1, element.positionY);
+                return new Element(element.position.positionX + 1, element.position.positionY);
 
             case (Direction.BAS_DIAGONAL_DROITE):
-                return new Element(element.positionX + 1, element.positionY - 1);
+                return new Element(element.position.positionX + 1, element.position.positionY - 1);
             default:
                 return new Element(100000000, 10000000000); // TODO revoir ce point
         }
@@ -104,64 +145,66 @@ export class Element {
     }
 
 
-    nextMove(element: Element): void {
-        const distance = this.getNbTourByOtherElement(element);
-        switch (this.type) {
-            case TypeElement.HERO:
-                if (distance <= this.getDistanceHop()) {
-                    this.positionX = element.positionX;
-                    this.positionY = element.positionY;
-                } else {
-                    const nextPosition: Element = this.getNextHop(element)
-                    this.positionX = nextPosition.positionX;
-                    this.positionY = nextPosition.positionY;
-                };
-                break;
+    // nextMove(element: Element): void {
+    //     const distance = this.getNbTourByOtherElement(element);
+    //     switch (this.type) {
+    //         case TypeElement.HERO:
+    //             if (distance <= this.getDistanceHop()) {
+    //                 this.positionX = element.positionX;
+    //                 this.positionY = element.positionY;
+    //             } else {
+    //                 const nextPosition: Element = this.getNextHop(element)
+    //                 this.positionX = nextPosition.positionX;
+    //                 this.positionY = nextPosition.positionY;
+    //             };
+    //             break;
 
-            case TypeElement.ZOMBIE:
-                break;
-            default:
-                break;
-        }
-    }
+    //         case TypeElement.ZOMBIE:
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
+
     getNextHop(cible: Element): Element {
-        // TODO a delete log
-        let nextNeighbourDebug: Element[] = []
-        //
-        let nextNeighbour = new Element(this.positionX, this.positionY);
+
+        let nextNeighbour = new Element(this.position.positionX, this.position.positionY);
         let isFinish = false;
         while (!isFinish) {
             let isFindNewNearestNeighbour = false;
             for (const direction in Direction) {
                 const elementDirectNeighbour = this.getNeigbourElementByDirection(nextNeighbour, <Direction>+direction);
-                if (elementDirectNeighbour.getDistanceManathanByElement(this) <= this.getDistanceHop()
+                if (elementDirectNeighbour.getDistanceManathanByElement(this) <= this.getDistanceHopMax()
                     && elementDirectNeighbour.getDistanceManathanByElement(cible) < nextNeighbour.getDistanceManathanByElement(cible)) {
                     nextNeighbour = elementDirectNeighbour;
                     isFindNewNearestNeighbour = true;
-
-                    // TODO a delete 
-                    nextNeighbourDebug.push(nextNeighbour);
                 }
             }
             if (!isFindNewNearestNeighbour) {
                 isFinish = true;
             }
         }
-
-        // TODO a delete 
-        console.debug(nextNeighbourDebug)
         return nextNeighbour;
     }
 
-    getDistanceHop(): number {
+    getDistanceHopMax(): number {
         switch (this.type) {
             case TypeElement.HERO:
                 return 1000;
             case TypeElement.ZOMBIE:
                 return 400;
             default:
-                return 10000000000; // TODO voir comment gerer ça
+                return UNREACHABLE_VALUE; // TODO voir comment gerer ça
         }
+    }
+}
+
+export class Position {
+    positionX: number;
+    positionY: number;
+    constructor(positionX, positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
     }
 }
 
@@ -169,7 +212,7 @@ export class Partie {
 
 
     makeADecision(): String {
-        let humansSauvable = this.humans.filter((humain) => {
+        let humansSauvable: Element[] = this.humans.filter((humain) => {
             const humanDistanceHero = humain.getNbTourByOtherElement(this.hero);
             const humanDistanceLePlusProcheZombie = humain.getDistanceOfElementLePlusProche(this.zombies);
             console.error(" humain sauvable");
@@ -186,7 +229,7 @@ export class Partie {
 
     fillZombies(zombieCount): void {
         for (let i = 0; i < zombieCount; i++) {
-            var inputs: string[] = readline().split(' ');
+            var inputs: string[] = readlineCustom().split(' ');
             const zombieId: number = parseInt(inputs[0]);
             const zombieX: number = parseInt(inputs[1]);
             const zombieY: number = parseInt(inputs[2]);
@@ -199,7 +242,7 @@ export class Partie {
 
     fillHumans(humanCount: number): void {
         for (let i = 0; i < humanCount; i++) {
-            var inputs: string[] = readline().split(' ');
+            var inputs: string[] = readlineCustom().split(' ');
             const humanId: number = parseInt(inputs[0]);
             const humanX: number = parseInt(inputs[1]);
             const humanY: number = parseInt(inputs[2]);
@@ -210,43 +253,63 @@ export class Partie {
 }
 
 
+
 export class Simulation {
 
-    simulationMap: EtatElementSimulation[];
-    score: number;
+    simulationMap: EtatElementSimulation[] = [];
+    score = 0;
     // A ajouter la strategie attendu par le hero
-    constructor() {
-        this.simulationMap = [];
-    }
+    strategieHero: StrategieHero = new StrategieHeroDefault();
 
     nextTurn() {
-        const humans = this.simulationMap.filter(humans => !humans.isDead && (humans.element.type === TypeElement.HUMAN || humans.element.type === TypeElement.HERO))
-        this.simulationMap.forEach((etat) => {
+        const humans = this.simulationMap.filter(human => !human.isDead && (human.element.type === TypeElement.HUMAN));
+        const zombies = this.simulationMap.filter(zombie => !zombie.isDead && (zombie.element.type === TypeElement.ZOMBIE));
+        const hero = this.simulationMap.find(zombie => !zombie.isDead && (zombie.element.type === TypeElement.HERO));
+        this.simulationMap.filter(etatElementSimulation => !etatElementSimulation.isDead).forEach((etat) => {
             switch (etat.element.type) {
                 case TypeElement.HERO:
+                    if (!!hero) {
+                        this.strategieHero.apply(hero, humans, zombies);
+                        const nbZombiesKilledInSameTurn = this.killAnyZombieInTheRange(hero, zombies);
+                        if(nbZombiesKilledInSameTurn>0){
+                            this.score += (Math.pow(humans.length,2) * 10) * calculFibonaci(nbZombiesKilledInSameTurn + 2)
+                        }
+                    }
                     break;
                 case TypeElement.ZOMBIE:
                     const positionInitial = etat.element;
                     const nearestHuman = findTheNearestHuman(positionInitial, humans);
                     if (!!nearestHuman) {
                         const nextPosition = positionInitial.getNextHop(nearestHuman.element);
-                        etat.element.positionX = nextPosition.positionX;
-                        etat.element.positionY = nextPosition.positionY;
+                        etat.element.position.positionX = nextPosition.position.positionX;
+                        etat.element.position.positionY = nextPosition.position.positionY;
                         if (etat.element.getDistanceManathanByElement(nearestHuman.element) === 0) {
                             nearestHuman.isDead = true;
                         }
                     }
-
                     break;
                 default:
                     break;
             }
         })
+        console.error('test ');
+    }
+
+
+
+    killAnyZombieInTheRange(hero: EtatElementSimulation, zombies: EtatElementSimulation[]): number {
+        let nbZombies = 0;
+        zombies.filter(zombie => zombie.element.getDistanceManathanByElement(hero.element) <= 2000)
+            .forEach(zombie => {
+                zombie.isDead = true;
+                nbZombies++;
+            });
+        return nbZombies;
     }
 }
 
 export class EtatElementSimulation {
-    element: Element
+    element: Element;
     isDead: boolean;
 
     constructor(type: TypeElement, element: Element) {
@@ -258,13 +321,13 @@ export class EtatElementSimulation {
 
 function initPartie(): Partie {
     let partie = new Partie();
-    var inputs: string[] = readline().split(' ');
+    var inputs: string[] = readlineCustom().split(' ');
     const x: number = parseInt(inputs[0]);
     const y: number = parseInt(inputs[1]);
     partie.hero = new Element(x, y, TypeElement.HERO);
-    const humanCount: number = parseInt(readline());
+    const humanCount: number = parseInt(readlineCustom());
     partie.fillHumans(humanCount)
-    const zombieCount: number = parseInt(readline());
+    const zombieCount: number = parseInt(readlineCustom());
     partie.fillZombies(zombieCount);
     return partie;
 }
@@ -283,9 +346,22 @@ while (true && !isModeDev) {
 function findTheNearestHuman(positionInitial: Element, humans: EtatElementSimulation[]): EtatElementSimulation | undefined {
     const humansElement = humans.map((element) => element.element);
     const nearestHuman = positionInitial.getElementLePlusProche(humansElement);
-    const etatElementHuman = humans.find((human => human.element.positionX === nearestHuman.positionX && human.element.positionY === nearestHuman.positionY));
+    const etatElementHuman = humans.find(
+        (human => human.element.position.positionX === nearestHuman.position.positionX &&
+            human.element.position.positionY === nearestHuman.position.positionY));
     return etatElementHuman;
 
 }
 
+
+export function calculFibonaci(indice: number): number {
+    if (indice == 0) {
+        return 0;
+    } else if (indice == 1) {
+        return 1;
+    }
+    else {
+        return calculFibonaci(indice - 1) + calculFibonaci(indice - 2);
+    }
+}
 
--- human vs zombies/tests/codingame.test.ts
+++ human vs zombies/tests/codingame.test.ts
@@ -1,4 +1,4 @@
-import { Partie, Element, TypeElement, Simulation, EtatElementSimulation } from '../src/codingame';
+import { Partie, Element, TypeElement, Simulation, EtatElementSimulation, calculFibonaci } from '../src/codingame';
 
 describe('codingame test ', () => {
 
@@ -15,48 +15,164 @@ describe('codingame test ', () => {
       //THEN
       expect(partie.makeADecision()).toBe("0 10");
     });
-
-
-    describe('test du simulateur', () => {
-      it('le zombie doit avancer vers l\'humain en restant sous sa limite de 400 de portée', () => {
-        // GIVEN 
-        let simulateur = new Simulation()
-        const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
-        simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
-        const initialHuman = new Element(10000, 10000, TypeElement.HUMAN);
-        simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
-        // WHEN
-        simulateur.nextTurn();
-        // THEN 
-        const zombieActualise = simulateur.simulationMap[0];
-        const HumainActualise = simulateur.simulationMap[1];
-        expect(HumainActualise.isDead).toBeFalsy();
-        expect(zombieActualise).toBeDefined();
-        expect(zombieActualise?.element).toBeDefined();
-        expect(zombieActualise?.element.positionX).toEqual(282);
-        expect(zombieActualise?.element.positionY).toEqual(283);
-        expect(zombieActualise?.element.getDistanceManathanByElement(initialZombie)).toBeLessThan(!!zombieActualise ? zombieActualise.element.getDistanceHop() : -1)
-      })
-
-      it('le zombie doit avancer vers l\'humain et le tuer ', () => {
-        // GIVEN 
-        let simulateur = new Simulation()
-        const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
-        simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
-        const initialHuman = new Element(10, 10, TypeElement.HUMAN);
-        simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
-        // WHEN
-        simulateur.nextTurn();
-        // THEN 
-        const zombieActualise = simulateur.simulationMap[0];
-        const humainActualise = simulateur.simulationMap[1];
-        expect(humainActualise.isDead).toBeTruthy();
-        expect(zombieActualise).toBeDefined();
-        expect(zombieActualise.element).toBeDefined();
-        expect(zombieActualise.element.positionX).toEqual(10);
-        expect(zombieActualise.element.positionY).toEqual(10);
-        expect(zombieActualise.element.getDistanceManathanByElement(initialZombie)).toBeLessThan(zombieActualise.element.getDistanceHop())
-      })
-    })
   });
+
+  describe('test du simulateur', () => {
+    it('le zombie doit avancer vers l\'humain en restant sous sa limite de 400 de portée', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialHuman = new Element(10000, 10000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const HumainActualise = simulateur.simulationMap[1];
+      expect(HumainActualise.isDead).toBeFalsy();
+      expect(zombieActualise).toBeDefined();
+      expect(zombieActualise?.element).toBeDefined();
+      expect(zombieActualise?.element.position.positionX).toEqual(282);
+      expect(zombieActualise?.element.position.positionY).toEqual(283);
+      expect(zombieActualise?.element.getDistanceManathanByElement(initialZombie)).toBeLessThan(!!zombieActualise ? zombieActualise.element.getDistanceHopMax() : -1)
+    })
+
+    it('le zombie doit avancer vers l\'humain et le tuer ', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialHuman = new Element(10, 10, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const humainActualise = simulateur.simulationMap[1];
+      expect(humainActualise.isDead).toBeTruthy();
+      expect(zombieActualise).toBeDefined();
+      expect(zombieActualise.element).toBeDefined();
+      expect(zombieActualise.element.position.positionX).toEqual(10);
+      expect(zombieActualise.element.position.positionY).toEqual(10);
+      expect(zombieActualise.element.getDistanceManathanByElement(initialZombie)).toBeLessThan(zombieActualise.element.getDistanceHopMax())
+    })
+
+
+    it('l\'hero doit tuer le zombie qui est dans sa range et avoir le score de 20 ', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialHuman = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      const hero = new Element(1000, 0, TypeElement.HERO);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HERO, hero));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const heroActualise = simulateur.simulationMap[1];
+      const humainActualise = simulateur.simulationMap[2];
+      expect(zombieActualise.isDead).toBeTruthy();
+      expect(simulateur.score).toBe(20);
+    })
+
+
+
+    it('l\'hero doit tuer les deux zombies qui est dans sa range et avoir le score de 120 ', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialZombie2 = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie2));
+
+      const initialZombie3 = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie3));
+
+      const initialHuman = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      const initialHuman2 = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman2));
+
+      const hero = new Element(1000, 0, TypeElement.HERO);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HERO, hero));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const zombieActualise2 = simulateur.simulationMap[1];
+      const zombieActualise3 = simulateur.simulationMap[2];
+      const heroActualise = simulateur.simulationMap[4];
+      const humainActualise = simulateur.simulationMap[5];
+      const humainActualise2 = simulateur.simulationMap[6];
+      expect(zombieActualise.isDead).toBeTruthy();
+      expect(simulateur.score).toBe(120);
+    })
+
+    it('l\'hero doit tuer les trois zombies qui est dans sa range et avoir le score de 120 ', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialZombie2 = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie2));
+
+      const initialZombie3 = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie3));
+
+      const initialHuman = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      const initialHuman2 = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman2));
+
+      const hero = new Element(1000, 0, TypeElement.HERO);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HERO, hero));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const zombieActualise2 = simulateur.simulationMap[1];
+      const heroActualise = simulateur.simulationMap[2];
+      const humainActualise = simulateur.simulationMap[3];
+      const humainActualise2 = simulateur.simulationMap[4];
+      expect(zombieActualise.isDead).toBeTruthy();
+      expect(simulateur.score).toBe(120);
+    })
+
+
+    it('le zombie et le hero se dirige vers l\'humain', () => {
+      // GIVEN 
+      let simulateur = new Simulation()
+      const initialZombie = new Element(0, 0, TypeElement.ZOMBIE);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.ZOMBIE, initialZombie));
+      const initialHuman = new Element(0, 5000, TypeElement.HUMAN);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HUMAN, initialHuman));
+      const hero = new Element(5000, 5000, TypeElement.HERO);
+      simulateur.simulationMap.push(new EtatElementSimulation(TypeElement.HERO, hero));
+      // WHEN
+      simulateur.nextTurn();
+      // THEN 
+      const zombieActualise = simulateur.simulationMap[0];
+      const heroActualise = simulateur.simulationMap[2];
+      const humainActualise = simulateur.simulationMap[1];
+      expect(zombieActualise.isDead).toBeFalsy();
+      expect(humainActualise.isDead).toBeFalsy();
+      expect(zombieActualise.element.position.positionX).toBe(0);
+      expect(zombieActualise.element.position.positionY).toBe(400);
+      expect(heroActualise.element.position.positionX).toBe(4000);
+      expect(heroActualise.element.position.positionY).toBe(5000);
+    })
+
+  })
+});
+
+
+describe('fibonacci', () => {
+
+  it('doit retourner 1 avec le parametre', () => {
+    expect(calculFibonaci(3)).toBe(2)
+  })
+
 })
